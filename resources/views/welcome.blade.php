5f079185a7e0737a5dd7ccdeed6525d68fc37867
Andrew Judd <awjudd@gmail.com>
2022-01-30 17:14:56 -0500

    Removing comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    <strong>{{ gethostname() }} - Blarg</strong>
+                                    <strong>{{ gethostname() }}</strong>
                                 </div>
                             </div>
                         </div>
