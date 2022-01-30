ecd87361d27c448927f228d7faacfda7099d920d
Andrew Judd <awjudd@gmail.com>
2022-01-30 15:31:55 -0500

    removing testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    <strong>{{ gethostname() }}</strong> - Testing
+                                    <strong>{{ gethostname() }}</strong>
                                 </div>
                             </div>
                         </div>
